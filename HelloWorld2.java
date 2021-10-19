a3408d7c95ccf68fa0af8b1031215716b1f378d0
noob1216 <90545128+noob1216@users.noreply.github.com>
2021-10-19 16:30:13 +0530

    Update HelloWorld2.java

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
   public static void main(String[] args)
   {
-  System.out.println("****Hello World & Welcome to J*****");
+  System.out.println("****Hello World & trying Webhook*****");
   }
 } 
